49ca817d39d3d5dca62e4ad299a0783d8cd6e7b9
vorzawk <vrabhilash92@gmail.com>
2017-11-26 18:52:06 -0600

    Evaluation of Expression Tree

--- searchCondition.cpp
+++ searchCondition.cpp
@@ -2,8 +2,20 @@
 #include <string>
 #include <sstream>
 using namespace std;
+#include <cstdlib>
 #include "parseTree.h"
 #include "common.h"
+#include "io.h"
+#include "Block.h"
+#include "Config.h"
+#include "Disk.h"
+#include "Field.h"
+#include "MainMemory.h"
+#include "Relation.h"
+#include "Schema.h"
+#include "SchemaManager.h"
+#include "Tuple.h"
+
 
 string switchDelimiter(string delimiter) {
     char delim;
@@ -80,6 +92,99 @@ node *createTree(char *searchStrBuf) {
     return root;
 }
 
+int hashString(string str) {
+    int sum=0;
+    for (auto it=str.begin(); it != str.end(); it++) {
+        sum *= 10;
+        sum += *it - '0';
+    }
+    return sum;
+}
+
+int evalArith(node *root, Tuple selTuple) {
+    if (root->subTree.empty()) {
+        string nodeStr = root->nodeType;
+        char firstchar = nodeStr[0];
+        if (isdigit(firstchar)) {
+            return hashString(nodeStr);
+        } else if (isalpha(firstchar)) {
+            Schema tuple_schema = selTuple.getSchema();
+            if (tuple_schema.getFieldType(nodeStr)==INT) {
+                return selTuple.getField(nodeStr).integer;
+            } else {
+                return hashString(*selTuple.getField(nodeStr).str);
+            }
+        } else if (nodeStr[0] == '"') {
+            string value = string(nodeStr.begin()+1, nodeStr.end()-1);
+            return hashString(value);
+        } else {
+            cerr << "evalArith: Invalid value read";
+        }
+    } else {
+        // We are sure that it is a binary operation
+        int leftValue = evalArith(root->subTree[0], selTuple);
+        int rightValue = evalArith(root->subTree[1], selTuple);
+        if (root->nodeType == "+") {
+            return leftValue + rightValue;
+        } else if (root->nodeType == "-") {
+            return leftValue - rightValue;
+        } else if (root->nodeType == "*") {
+            return leftValue * rightValue;
+        } else {
+            cerr << "evalArith: Invalid arithmetic operator";
+        }
+    }
+}
+
+bool evalBool(node *root, Tuple selTuple) {
+    if (root->nodeType == "OR") {
+        bool leftCond = evalBool(root->subTree[0], selTuple);
+        if (leftCond) {
+            return true;
+        }
+        bool rightCond = evalBool(root->subTree[1], selTuple);
+        if (rightCond) {
+            return true;
+        }
+        return false;
+    } else if (root->nodeType == "AND") {
+        bool leftCond = evalBool(root->subTree[0], selTuple);
+        if (!leftCond) {
+            return false;
+        }
+        bool rightCond = evalBool(root->subTree[1], selTuple);
+        if (!rightCond) {
+            return false;
+        }
+        return true;
+    } else if (root->nodeType == "<") {
+        int leftValue = evalArith(root->subTree[0], selTuple);
+        int rightValue = evalArith(root->subTree[1], selTuple);
+        if (leftValue < rightValue) {
+            return true;
+        }
+        return false;
+    } else if (root->nodeType == ">") {
+        int leftValue = evalArith(root->subTree[0], selTuple);
+        int rightValue = evalArith(root->subTree[1], selTuple);
+        if (leftValue > rightValue) {
+            return true;
+        }
+        return false;
+    } else if (root->nodeType == "=") {
+        int leftValue = evalArith(root->subTree[0], selTuple);
+        int rightValue = evalArith(root->subTree[1], selTuple);
+        if (leftValue == rightValue) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+
+
 int main() {
     char *buf;
     node *root;
@@ -87,6 +192,9 @@ int main() {
     fgets(buf,50,stdin);
     root = createTree(buf);
     printTree(root, 0);
+    Tuple tuple;
+    bool ans = evalBool(root->subTree[0], tuple);
+    cout << ans << endl;
 }
 
 
